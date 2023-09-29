035014ed66166841f70e7cef9ae950ddef8e6078
Jake Elsberry <jelsberry96@gmail.com>
2023-09-29 12:53:47 -0700

    add post and get methods to home controller

--- Pierre/Controllers/HomeController.cs
+++ Pierre/Controllers/HomeController.cs
@@ -6,10 +6,23 @@ namespace Pierre.Controllers
   public class HomeController : Controller
   {
 
-    [Route("/")]
+    [HttpGet("/")]
     public ActionResult Index()
     {
       return View();
+    }
+
+    [HttpGet("/orders/new")]
+    public ActionResult CreateForm()
+    {
+      return View();
+    }
+
+    [HttpPost("/order")]
+    public ActionResult Create(string description)
+    {
+      Order myOrder = new Order(description);
+      return View("Index", myOrder);
     }
 
 
