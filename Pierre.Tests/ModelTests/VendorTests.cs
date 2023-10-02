441e785115e29b05cf40c6c22b5eb55d11c7db32
Jake Elsberry <jelsberry96@gmail.com>
2023-10-01 17:50:15 -0700

    write/pass test2 - Vendor constructor returns name

--- Pierre.Tests/ModelTests/VendorTests.cs
+++ Pierre.Tests/ModelTests/VendorTests.cs
@@ -5,14 +5,28 @@ using System;
 
 namespace Pierre.Tests
 {
-  [TestClass]
-  public class VendorTests
-  {
-    [TestMethod]
-    public void VendorConstructor_CreatesInstanceOfVendor_Vendor() 
+    [TestClass]
+    public class VendorTests
     {
-      Vendor newVendor = new Vendor("test vendor");
-      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+        [TestMethod]
+        public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+        {
+            Vendor newVendor = new Vendor("test vendor");
+            Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+        }
+
+        [TestMethod]
+        public void GetNameVendor_ReturnsVendorName_String()
+        {
+            //Arrange
+            string vendorName = "Test Vendor";
+            Vendor newVendor = new Vendor(vendorName);
+
+            //Act
+            string result = newVendor.Name;
+
+            //Assert
+            Assert.AreEqual(vendorName, result);
+        }
     }
-    }
-  }
+}
